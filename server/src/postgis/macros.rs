4879fbed72838bd058586c2d5ed4de7cb6d7d306
AM Smith <smitham42@gmail.com>
2023-05-31 14:05:46 -0700

    chore: reviewer comments

--- server/src/postgis/macros.rs
+++ server/src/postgis/macros.rs
@@ -1,33 +1,3 @@
 //! log macro's for postgis logging
-
-/// Writes a debug! message to the app::postgis logger
-#[macro_export]
-macro_rules! postgis_debug {
-    ($($arg:tt)+) => {
-        log::debug!(target: "app::postgis", $($arg)+)
-    };
-}
-
-/// Writes an info! message to the app::postgis logger
-#[macro_export]
-macro_rules! postgis_info {
-    ($($arg:tt)+) => {
-        log::info!(target: "app::postgis", $($arg)+)
-    };
-}
-
-/// Writes an warn! message to the app::postgis logger
-#[macro_export]
-macro_rules! postgis_warn {
-    ($($arg:tt)+) => {
-        log::warn!(target: "app::postgis", $($arg)+)
-    };
-}
-
-/// Writes an error! message to the app::postgis logger
-#[macro_export]
-macro_rules! postgis_error {
-    ($($arg:tt)+) => {
-        log::error!(target: "app::postgis", $($arg)+)
-    };
-}
+use lib_common::log_macros;
+log_macros!("postgis");
