e4bb33a68733d39dac50ad573705283ee0ba63ac
AM Smith <smitham42@gmail.com>
2023-05-31 17:38:02 -0700

    feat: add no-fly zone update

--- server/src/postgis/README.md
+++ server/src/postgis/README.md
@@ -4,19 +4,27 @@
 
 Aircraft routing and automated air traffic control (ATC) is performed with the help of a PostGIS database.
 
-In the current implementation, route nodes are populated from a list of vertiports and aviation waypoints.
+In the current implementation, route nodes are populated from a list of vertiports and aviation waypoints. Reports from `svc-compliance` will be used to build no-fly geometries (either permanent or temporary). 
 
 ## :crystal_ball: Upcoming
 
 These nodes will then be used to build routes. All nodes within 300 kilometers of one another will have a route. In the future, routes may be predetermined by the FAA, EASA, or other civil aviation agency (CAA).
 
-Reports from `svc-compliance` will be used to build no-fly geometries (either permanent or temporary). Routes that overlap with no-fly geometries at a customer's proposed flight time are discarded.
+Routes that overlap with no-fly geometries at a customer's proposed flight time are discarded.
 
 A shortest path algorithm (A*) can then connect the departure vertiport to the arrival vertiport. The list of waypoints is used to estimate a travel distance, duration, and (in coming releases) battery discharge.
 
 These routes are planned to be expanded to include multiple vertically stacked flight corridors, each with a pair of separated lanes for travel in opposing directions. Such corridors are mentioned by the FAA UAM CONOPS v2.0 (figures 4, 5, 6, and 7).
 
 Additionally, these corridors can be assigned a max capacity. Corridors at max capacity would be disregarded by the shortest path algorithm. This also allows for load balancing, prioritizing corridors with less utilization to spread traffic evenly.
+
+## Initialization
+
+The `scripts/postgis-init.sh` script creates the root CA and client certificate and key. These should not be used in production.
+
+The `scripts/init.sql` script is launched automatically when the `postgis` container is started (see `docker-compose.yml`).
+
+Use `docker compose down --volumes` to delete the local `postgis-ssl` and `postgis-data` volumes if changes have been made to either of these scripts.
 
 ## :elephant: PostgreSQL Database
 
@@ -27,6 +35,7 @@ The `arrow` schema defines the following tables:
 | Table | Description | 
 | ---- | ---- |
 | [`rnodes`](#pushpin-rnodes) | This table lists nodes through which aircraft can route.<br>Node types currently includes:<br>- Waypoints<br>- Vertiports |
+| [`nofly`](#no_entry-nofly) | This table lists no-fly zones. These can be temporary or permanent. They can be vertiports who shouldn't be flown over unless they are the destination or departure port. |
 
 ### :pushpin: `rnodes`
 
@@ -36,3 +45,14 @@ The `arrow` schema defines the following tables:
 | **arrow_id (PK)** | UUID UNIQUE | The Arrow UUID identifier of this waypoint or vertiport. |
 | node_type | enum ('waypoint', 'vertiport') | The type of route node this represents. |
 | geom | GEOMETRY(Point) | The latitude and longitude of this node, altitude ignored. | 
+
+## :no_entry: `nofly`
+
+| Column | Type | Description |
+| ---- | ---- | --- | 
+| id | SERIAL | Unique integer identifier of the node, required for pgRouting. |
+| label | VARCHAR | The NOTAM identifier or other label unique to this no-fly zone.
+| geom | GEOMETRY | The polygon defining the bounds of this no-fly zone
+| time_start | TIMESTAMPTZ | The time that this no-fly zone becomes active. NULL if active by default, starting the moment it is created.
+| time_end | TIMESTAMPTZ | The time that this no-fly zone becomes inactive. NULL if no scheduled end date.
+| vertiport_id | UUID UNIQUE | The Arrow UUID identifier of this vertiport, if the no-fly is specifically for a vertiport. |
--- server/src/postgis/node.rs
+++ server/src/postgis/node.rs
@@ -2,6 +2,11 @@
 //! Nodes are columns in space that aircraft can fly between.
 //! Nodes can include fixed aviation coordinates (waypoints) or vertiports.
 
+use crate::grpc::server::grpc_server;
+use crate::postgis::node::Node as GisNode;
+use crate::postgis::node::NodeType as GisNodeType;
+use grpc_server::Node as RequestNode;
+
 #[derive(Debug, Clone, Copy)]
 /// The type of node (vertiport, waypoint, etc.)
 pub enum NodeType {
@@ -21,6 +26,29 @@ impl std::fmt::Display for NodeType {
     }
 }
 
+/// Possible conversion errors from the GRPC type to GIS type
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum NodeError {
+    /// Invalid UUID
+    BadUuid,
+
+    /// Invalid Node Type
+    UnrecognizedType,
+
+    /// No location provided
+    NoLocation,
+}
+
+impl std::fmt::Display for NodeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            NodeError::UnrecognizedType => write!(f, "Invalid node type provided."),
+            NodeError::BadUuid => write!(f, "Invalid node UUID provided."),
+            NodeError::NoLocation => write!(f, "No location was provided."),
+        }
+    }
+}
+
 #[derive(Debug, Copy, Clone)]
 /// Nodes that aircraft can fly between
 pub struct Node {
@@ -28,13 +56,57 @@ pub struct Node {
     pub uuid: uuid::Uuid,
 
     /// The latitude of the node
-    pub latitude: f64,
+    pub latitude: f32,
 
     /// The longitude of the node
-    pub longitude: f64,
+    pub longitude: f32,
 
     /// The type of node (vertiport, waypoint, etc.)
     pub node_type: NodeType,
+}
+
+/// Convert nodes from the GRPC request into nodes for the GIS database,
+///  detecting invalid arguments and returning an error if necessary.
+pub fn nodes_grpc_to_gis(req_nodes: Vec<RequestNode>) -> Result<Vec<GisNode>, NodeError> {
+    let mut nodes: Vec<GisNode> = vec![];
+    for node in &req_nodes {
+        let uuid = match uuid::Uuid::parse_str(&node.uuid) {
+            Ok(uuid) => uuid,
+            Err(e) => {
+                postgis_error!("(nodes_grpc_to_gis) failed to parse uuid: {}", e);
+                return Err(NodeError::BadUuid);
+            }
+        };
+
+        let node_type = match node.node_type {
+            x if x == (grpc_server::NodeType::Vertiport as i32) => GisNodeType::Vertiport,
+            y if y == (grpc_server::NodeType::Waypoint as i32) => GisNodeType::Waypoint,
+            e => {
+                postgis_error!("(update_node) invalid node type: {}", e);
+                return Err(NodeError::UnrecognizedType);
+            }
+        };
+
+        let (latitude, longitude) = match &node.location {
+            Some(l) => (l.latitude, l.longitude),
+            _ => {
+                postgis_error!("(update_node) no location provided for node.");
+                return Err(NodeError::NoLocation);
+            }
+        };
+
+        // TODO(R4): Check if lat, lon inside geofence for this region
+        let node = GisNode {
+            uuid,
+            latitude,
+            longitude,
+            node_type,
+        };
+
+        nodes.push(node);
+    }
+
+    Ok(nodes)
 }
 
 /// Updates nodes in the PostGIS database.
@@ -64,4 +136,91 @@ pub async fn update_nodes(nodes: Vec<Node>, pool: deadpool_postgres::Pool) -> Re
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::grpc::server::grpc_server::Coordinates;
+
+    #[test]
+    fn ut_nodes_request_to_gis_valid() {
+        let request_nodes: Vec<RequestNode> = vec![
+            RequestNode {
+                uuid: uuid::Uuid::new_v4().to_string(),
+                location: Some(Coordinates {
+                    latitude: 0.0,
+                    longitude: 0.0,
+                }),
+                node_type: grpc_server::NodeType::Vertiport as i32,
+            },
+            RequestNode {
+                uuid: uuid::Uuid::new_v4().to_string(),
+                location: Some(Coordinates {
+                    latitude: 1.0,
+                    longitude: 1.0,
+                }),
+                node_type: grpc_server::NodeType::Waypoint as i32,
+            },
+        ];
+
+        let nodes = match nodes_grpc_to_gis(request_nodes.clone()) {
+            Ok(nodes) => nodes,
+            Err(_) => panic!("Failed to convert nodes."),
+        };
+
+        assert_eq!(nodes.len(), request_nodes.len());
+
+        for (i, node) in nodes.iter().enumerate() {
+            let Some(location) = request_nodes[i].location else {
+                panic!();
+            };
+
+            assert_eq!(node.uuid.to_string(), request_nodes[i].uuid);
+            assert_eq!(node.latitude, location.latitude);
+            assert_eq!(node.longitude, location.longitude);
+        }
+    }
+
+    #[test]
+    fn ut_nodes_request_to_gis_invalid_uuid() {
+        let request_nodes: Vec<RequestNode> = vec![RequestNode {
+            uuid: "invalid".to_string(),
+            location: Some(Coordinates {
+                latitude: 0.0,
+                longitude: 0.0,
+            }),
+            node_type: grpc_server::NodeType::Vertiport as i32,
+        }];
+
+        let result = nodes_grpc_to_gis(request_nodes).unwrap_err();
+        assert_eq!(result, NodeError::BadUuid);
+    }
+
+    #[test]
+    fn ut_nodes_request_to_gis_invalid_node_type() {
+        let request_nodes: Vec<RequestNode> = vec![RequestNode {
+            uuid: uuid::Uuid::new_v4().to_string(),
+            location: Some(Coordinates {
+                latitude: 0.0,
+                longitude: 0.0,
+            }),
+            node_type: grpc_server::NodeType::Vertiport as i32 + 1,
+        }];
+
+        let result = nodes_grpc_to_gis(request_nodes).unwrap_err();
+        assert_eq!(result, NodeError::UnrecognizedType);
+    }
+
+    #[test]
+    fn ut_nodes_request_to_gis_invalid_location() {
+        let request_nodes: Vec<RequestNode> = vec![RequestNode {
+            uuid: uuid::Uuid::new_v4().to_string(),
+            location: None,
+            node_type: grpc_server::NodeType::Vertiport as i32,
+        }];
+
+        let result = nodes_grpc_to_gis(request_nodes).unwrap_err();
+        assert_eq!(result, NodeError::NoLocation);
+    }
 }
