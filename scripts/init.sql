5b6be2bf5215fbfa9d7b69242b443c72f2298d4a
A.M. Smith <smitham42@gmail.com>
2024-04-13 15:53:57 -0700

    fix: postgis no longer in arrow schema

--- scripts/init.sql
+++ scripts/init.sql
@@ -6,16 +6,9 @@ REVOKE ALL ON SCHEMA public FROM PUBLIC;
 
 CREATE SCHEMA IF NOT EXISTS arrow;
 CREATE EXTENSION postgis CASCADE;
-UPDATE pg_extension
-    SET extrelocatable = TRUE
-    WHERE extname = 'postgis';
-
-ALTER EXTENSION postgis
-    SET SCHEMA arrow;
-
-CREATE EXTENSION postgis_sfcgal SCHEMA arrow;
+CREATE EXTENSION postgis_sfcgal CASCADE;
 
 SET search_path TO "$user", arrow, postgis, topology, public;
 ALTER ROLE svc_gis SET search_path TO "$user", arrow, postgis, topology, public;
-
 GRANT ALL PRIVILEGES ON SCHEMA arrow TO svc_gis;
+ALTER DATABASE gis OWNER TO svc_gis;
